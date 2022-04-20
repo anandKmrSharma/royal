a04250b3299e9f2188a3500127266c477f5b7b6c
anandKmrSharma <87422058+anandKmrSharma@users.noreply.github.com>
2022-04-20 15:43:06 +0530

    up

--- royalbro/src/Components/Data.jsx
+++ royalbro/src/Components/Data.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 // import {useEffect} from "react";
 import {useSelector,useDispatch} from "react-redux";
 import{storeData} from '../Redux/action'
-// import { store } from "../Redux/store";
+import { store } from "../Redux/store";
 
 
 const Data= ()=>{
@@ -12,14 +12,14 @@ const Data= ()=>{
     useEffect( ()=>{
         fetch("https://jsonplaceholder.typicode.com/posts")
         .then((res)=> res.json())
-        //.then((res)=> dispatch(storeData(res)))
+        // .then((res)=> console.log(res))
         .then((res)=> dispatch(storeData(res)))
 
         .catch((err)=> console.log(err));
     }, [])
     
     const data= useSelector((state)=> state.data );
-    console.log(data);
+    console.log("data",data);
 
     return <div>
         <h1> details of the data</h1>
--- royalbro/src/index.js
+++ royalbro/src/index.js
@@ -11,11 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <provider store={store}>
+    <Provider store={store}>
 
     <App/>
     
-    </provider>
+    </Provider>
   </React.StrictMode>
 );
 
